d6bca9d43c90c528493a32d9d84dc36456491c8b
lzam <110193626+lzam0@users.noreply.github.com>
2026-09-17 13:03:28 +0100

    Add: is-anagram - submission-10

---
+++ Data Structures & Algorithms/is-anagram/submission-10.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        dictS, dictT = {},{}
+
+        # if lenghts are the same for strings
+        if len(s) != len(t):
+            return False
+
+        for i in range(len(s)):
+            if s[i] in dictS:
+                dictS[s[i]] += 1
+            else:
+                dictS[s[i]] = 1
+
+        for i in range(len(t)):
+            if t[i] in dictT:
+                dictT[t[i]] += 1
+            else:
+                dictT[t[i]] = 1
+
+
+        if dictT == dictS:
+            return True
+        
+        return False
