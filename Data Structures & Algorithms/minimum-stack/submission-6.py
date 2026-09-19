0efc19bf821914e20a56950f3c53f2f98ca0bb60
lzam <110193626+lzam0@users.noreply.github.com>
2026-09-19 23:24:18 +0100

    Add: minimum-stack - submission-6

---
+++ Data Structures & Algorithms/minimum-stack/submission-6.py
@@ -0,0 +1,30 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        # print("before push",self.minStack, self.stack)
+        self.stack.append(val)
+
+        # check if the stack is empty
+        if len(self.minStack) == 0:
+            # append it to the minStack
+            self.minStack.append(val)
+        else:
+            # compare between val and the most recent value within stack
+            # append the smallest value to stack
+            self.minStack.append(min(val, self.minStack[-1]))
+        # print(self.minStack, self.stack)
+
+    def pop(self) -> None:
+        # remove the most recent value of stack
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
