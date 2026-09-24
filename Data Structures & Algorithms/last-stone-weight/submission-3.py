554f34811807d0a67586425a1af7e8fb53120672
lzam <110193626+lzam0@users.noreply.github.com>
2026-09-24 01:05:58 +0100

    Add: last-stone-weight - submission-3

---
+++ Data Structures & Algorithms/last-stone-weight/submission-3.py
@@ -0,0 +1,33 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        
+        new_arr = []
+        
+        # turn the stone arr into -ve
+        for stone in stones:
+            new_arr.append(-stone)
+
+        print(new_arr)
+
+        # turn the new arr into a heap
+        heapq.heapify(new_arr)
+
+        # heap new arr
+        print(new_arr)
+        
+        while len(new_arr) > 1:
+            # obtain the 'largest' ones
+            first = heapq.heappop(new_arr)
+            second = heapq.heappop(new_arr)
+
+
+            # check if the two stones can be smashed
+            if first != second:
+                # smash together
+                heapq.heappush(new_arr, first - second)
+            
+            print(new_arr)
+        
+        # repeat again
+        new_arr.append(0)
+        return -new_arr[0]
